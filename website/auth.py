3a6e7d66383f1c81e1773c777d7a12f4eb6eafcf
Can Dilikoglu <55206720+candilikoglu@users.noreply.github.com>
2022-04-25 14:32:49 -0400

    Update Inmate and Add jailor

--- website/auth.py
+++ website/auth.py
@@ -12,49 +12,26 @@ ADMIN = 'Admin'
 def login():
     if request.method == 'POST':
         login_information = (
-            request.form['Username'],
-            request.form['Password']
+            request.form.get('Username'),
+            request.form.get('Password')
         )
         if len(request.form['Username']) <= 0:
             flash('Please enter username', category='error')
         elif len(request.form['Password']) <=0:
             flash('Please enter password', category='error')
-        elif databaseFunctions.check_if_attribute_exists(JAILOR, login_information[0], login_information[1]):        
+        elif databaseFunctions.check_if_user_exists(JAILOR, login_information[0], login_information[1]):        
             return render_template('home.html', occupation=JAILOR)
-        elif databaseFunctions.check_if_attribute_exists(GUARD, login_information[0], login_information[1]):
+        elif databaseFunctions.check_if_user_exists(GUARD, login_information[0], login_information[1]):
             return render_template('home.html', occupation=GUARD)
-        elif databaseFunctions.check_if_attribute_exists(ADMIN, login_information[0], login_information[1]):
+        elif databaseFunctions.check_if_user_exists(ADMIN, login_information[0], login_information[1]):
             return render_template('home.html', occupation= ADMIN)
         else:
             flash('Login information is incorrect', category='error')
     
     return render_template('login.html')
 
-
 @auth.route('/logout')
 def logout():
     if 'occupation' in databaseFunctions.navbardict:
         del databaseFunctions.navbardict['occupation']
     return render_template('login.html')
-
-@auth.route('/createUser', methods=['POST', 'GET'])
-def createUser():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        fullName = request.form.get('fullName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
-
-        if len(email) < 6:
-            flash('Email must be greater than 6 characters', category='error')
-        elif password1 != password2:
-            flash('Passwords must match', category='error')
-        elif len(password1) < 7:
-            flash('Password must be greater than 7 characters', category = 'error')
-        elif len(fullName) < 4:
-            flash('Full name must be greater than 4 characters', category = 'error')
-        else:
-            flash('Account created successfully', category='success')
-    
-    occupation = databaseFunctions.navbardict['occupation']       
-    return render_template('createUser.html', occupation=occupation)
--- website/views.py
+++ website/views.py
@@ -29,18 +29,18 @@ def addInmate():
     elif  request.method == 'POST':
         occupation = databaseFunctions.navbardict['occupation']
         inmate_details = (
-            request.form['InmateID'],
-            request.form['Fullname'],
-            request.form['DOB'],
-            request.form['Address'],
-            request.form['Sentence'],
-            request.form['Crime'],
-            request.form['FIR']
+            request.form.get('InmateID'),
+            request.form.get('Fullname'),
+            request.form.get('DOB'),
+            request.form.get('Address'),
+            request.form.get('Sentence'),
+            request.form.get('Crime'),
+            request.form.get('FIR')
         )
         
         if len(inmate_details[0]) <= 0:
             flash('Inmate ID can not be empty', category='error')
-        elif databaseFunctions.check_if_ID_exists(inmate_details[0], 'Inmate'):
+        elif databaseFunctions.check_if_ID_exists(inmate_details[0], 'Inmate', 'InmateID'):
             flash('Inmate ID already exists',category='error')
         elif len(inmate_details[1]) <= 0:
             flash('Fullname can not be empty',category='error')
@@ -55,3 +55,80 @@ def addInmate():
             return render_template('inmate.html', inmate_info = inmate_info, occupation = occupation)
         
     return render_template('addInmate.html', FIRID = FIRID, occupation = occupation)
+
+@views.route('/addJailor', methods=['POST', 'GET'])
+def addJailor():
+    
+    FIRID = databaseFunctions.get_FIRID()
+    InmateID = databaseFunctions.get_InmateID()
+    
+    if request.method == 'GET':
+        occupation = databaseFunctions.navbardict['occupation']
+        return render_template('addJailor.html', FIRID = FIRID, InmateID = InmateID, occupation = occupation)
+    
+    if request.method == 'POST':
+        newjailor_information = (
+            request.form.get('JailorID'),
+            request.form.get('Fullname'),
+            request.form.get('Address'),
+            request.form.get('Shift'),
+            request.form.get('Inmates'),
+            request.form.get('FIR'),
+            request.form.get('Username'),
+            request.form.get('password1'),            
+        )       
+        
+        password2  = request.form['password2']
+        
+        if  len(newjailor_information[0]) <= 0:
+            flash('Jailor ID can not be empty', category='error')
+        elif databaseFunctions.check_if_ID_exists(newjailor_information[0], 'Jailor', 'JailorID'):
+            flash('A jailor with this ID already exists', category='error')
+        elif len(newjailor_information[1]) <=0:
+            flash('Fullname can not be empty', category='error')
+        elif len(newjailor_information[6]) <= 0:
+            flash('Username can not be empty', category='error')
+        elif len(newjailor_information[7]) <= 0:
+            flash('Password can not be empty', category='error')
+        elif newjailor_information[7] != password2:
+            flash('Passwords do not match', category='error')
+        else:
+            flash('Jailor has been added successfully',category='success')
+            databaseFunctions.insert_jailor(newjailor_information)
+            
+        
+    occupation = databaseFunctions.navbardict['occupation']       
+    return render_template('addJailor.html', occupation=occupation)
+
+@views.route('/updateInmate/<int:inmate_id>', methods=['POST', 'GET'])
+def updateInmate(inmate_id):
+    FIRID = databaseFunctions.get_FIRID()
+    inmate_info = databaseFunctions.get_inmate_information_with_ID(str(inmate_id))
+    occupation = databaseFunctions.navbardict['occupation']
+    
+    if request.method == 'GET':
+        return render_template('updateInmate.html', inmate_info = inmate_info, FIRID=FIRID)
+    else:
+        inmate_details = (
+            request.form.get('InmateID'),
+            request.form.get('Fullname'),
+            request.form.get('DOB'),
+            request.form.get('Address'),
+            request.form.get('Sentence'),
+            request.form.get('Crime'),
+            request.form.get('FIR')
+        )
+        
+        if len(inmate_details[3]) <= 0:
+            flash('Sentence can not be empty', category='error')
+        elif len(inmate_details[4]) <= 0:
+            flash('Crime can not be empty', category='error')
+        else:
+            flash('Inmate Information has been successfully changed',category='success')
+            databaseFunctions.delete_inmate(str(inmate_id))
+            databaseFunctions.insert_inmate(inmate_details)
+            inmate_info = databaseFunctions.query_inmate_information()
+            return render_template('inmate.html', inmate_info = inmate_info, occupation = occupation)
+        
+
+    
